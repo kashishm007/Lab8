d099029628a39715fb83e041a790b470b4aa8f46
kashishm007 <50669884+kashishm007@users.noreply.github.com>
2022-10-25 17:59:52 -0600

    DeleteCity added

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -69,8 +69,8 @@ public class CustomList extends ArrayAdapter<City> {
         }
     }
 
-//    public void deleteCity(City city){
-//        cities.remove(city);
-//    }
+    public void deleteCity(City city){
+        cities.remove(city);
+    }
 
 }
